eea15173cff4b1b61affb15d9509c01cba647919
Ethan Nelson <git@ethan-nelson.com>
2017-02-14 21:39:00 -0600

    Add daily data

---
+++ README.md
@@ -0,0 +1,9 @@
+GPCP Scripts
+============
+
+This repository contains Python functions to read [Global Precipitation Climatology Project](https://precip.gsfc.nasa.gov/) version 2.2 files. See the script for documentation on running it.
+
+Current methods included are:
+
+* `read_monthly_data`, which reads GPCP monthly data into a combined array.
+* `read_daily_data`, which reads a month of GPCP daily data into a combined array.
--- gpcp_tools.py
+++ gpcp_tools.py
@@ -53,3 +53,61 @@ def read_monthly_data(file_dir, years):
             data[i,m,:,:] = data_unpack
 
     return data
+
+
+def read_daily_data(file_dir, year, month):
+    """Reads GPCP daily data into an array.
+
+    Parameters
+    ----------
+    file_dir : str
+        Base directory structure where files are located with trailing slash.
+
+    year : int or str
+        File year to read in.
+        
+    month : int or str
+        File month to read in.
+
+    Returns
+    -------
+    data : arr with shape [31, 72, 144]
+        Array of rainfall values for each day in [mm/day]. Months that are
+        not 31 days long are padded with zeroes on the end and the user is
+        alerted to this early termination.
+
+    References
+    ----------
+    Header and grid size information taken from GPCP documentation [1].
+
+    .. [1] Huffman, G. J. and Bolvin, D. T., 2013: GPCP Version 1.2 One-
+       Degree Daily Precipitation Data Set Documentation. Taken from:
+       ftp://meso.gsfc.nasa.gov/pub/1dd-v1.2/1DD_v1.2_doc.pdf.
+    """
+
+    data = np.zeros([31, 180, 360])
+    lats = np.arange(89.5, -90, -1)
+    lons = np.arange(0.5, 360, 1)
+
+    grid_size = 360 * 180  # size of grid to read one day
+    read_size = grid_size * 4  # bit count for grid_size to advance one day
+    structure = ">" + ('f' * grid_size)
+
+    fname = file_dir + 'gpcp_1dd_v1.2_p1d.' + year + month.zfill(2)
+    f = open(fname,'r')
+    f.seek(0)
+
+    header = f.read(1440)
+
+    for m in range(31):
+        try:
+            read_data = f.read(read_size)
+
+            data_unpack = struct.unpack(structure, read_data)
+            data_unpack = np.reshape(data_unpack, [180,360])
+            data[m,:,:] = data_unpack
+        except:
+            print 'Month has finished at %i.' % (m,)
+            break
+
+    return data
